e6bef7ab47bc10df8e97ad44313b5c57f67a0119
ricap18 <120043393+ricap18@users.noreply.github.com>
2023-04-14 16:00:48 +0200

    Corregido1

--- YouTube.sql
+++ YouTube.sql
@@ -15,83 +15,13 @@ CREATE SCHEMA IF NOT EXISTS `YouTube` DEFAULT CHARACTER SET utf8 ;
 USE `YouTube` ;
 
 -- -----------------------------------------------------
--- Table `YouTube`.`me_gusta_no_me_gusta`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `YouTube`.`me_gusta_no_me_gusta` (
-  `id_gusta_no_gusta` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `opcion` ENUM('gusta', 'noGusta') NOT NULL,
-  `fecha_creacion` DATE NOT NULL,
-  PRIMARY KEY (`id_gusta_no_gusta`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `YouTube`.`Comentarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `YouTube`.`Comentarios` (
-  `id_comentario` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_gusta_no_gusta` SMALLINT UNSIGNED NULL,
-  `texto_del_comentario` VARCHAR(100) NOT NULL,
-  `fecha_hora` DATETIME NOT NULL,
-  PRIMARY KEY (`id_comentario`),
-  INDEX `fk_comentarios_gusta_no_gusta_idx` (`id_gusta_no_gusta` ASC) ,
-  CONSTRAINT `fk_comentarios_gusta_no_gusta`
-    FOREIGN KEY (`id_gusta_no_gusta`)
-    REFERENCES `YouTube`.`me_gusta_no_me_gusta` (`id_gusta_no_gusta`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `YouTube`.`Canal`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `YouTube`.`Canal` (
-  `id_canal` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_usuario` SMALLINT UNSIGNED NOT NULL,
-  `nombre_canal` VARCHAR(45) NOT NULL,
-  `descripcion` VARCHAR(45) NOT NULL,
-  `fecha_creacion` DATE NOT NULL,
-  PRIMARY KEY (`id_canal`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `YouTube`.`Suscripciones`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `YouTube`.`Suscripciones` (
-  `id_suscripcion` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_canal` SMALLINT UNSIGNED NOT NULL,
-  PRIMARY KEY (`id_suscripcion`),
-  INDEX `id_suscripcion_canal_idx` (`id_canal` ASC) ,
-  CONSTRAINT `id_suscripcion_canal`
-    FOREIGN KEY (`id_canal`)
-    REFERENCES `YouTube`.`Canal` (`id_canal`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `YouTube`.`PlayList`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `YouTube`.`PlayList` (
-  `id_playList` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nombre_playlist` VARCHAR(45) NOT NULL,
-  `fecha_creacion` DATE NOT NULL,
-  `estado` ENUM('publico', 'privado') NOT NULL,
-  PRIMARY KEY (`id_playList`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `YouTube`.`Usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `YouTube`.`Usuarios` (
-  `id_usuario` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_comentario` SMALLINT UNSIGNED NOT NULL,
-  `id_subscripcion` SMALLINT UNSIGNED NOT NULL,
-  `id_playList` SMALLINT UNSIGNED NOT NULL,
+  `id_usuario` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_comentario` INT UNSIGNED NOT NULL,
+  `id_subscripcion` INT UNSIGNED NOT NULL,
+  `id_playList` INT UNSIGNED NOT NULL,
   `email` VARCHAR(45) NOT NULL,
   `password` VARCHAR(10) NOT NULL,
   `nombre` VARCHAR(45) NOT NULL,
@@ -99,25 +29,7 @@ CREATE TABLE IF NOT EXISTS `YouTube`.`Usuarios` (
   `sexo` ENUM('H', 'M') NOT NULL,
   `pais` VARCHAR(25) NOT NULL,
   `codigo_postal` CHAR(5) NOT NULL,
-  PRIMARY KEY (`id_usuario`),
-  INDEX `fk_usuarios_comentarios_idx` (`id_comentario` ASC) ,
-  INDEX `fk_usuarios_subscripcion_idx` (`id_subscripcion` ASC) ,
-  INDEX `fk_usuarios_playList_idx` (`id_playList` ASC) ,
-  CONSTRAINT `fk_usuarios_comentarios`
-    FOREIGN KEY (`id_comentario`)
-    REFERENCES `YouTube`.`Comentarios` (`id_comentario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_usuarios_subscripcion`
-    FOREIGN KEY (`id_subscripcion`)
-    REFERENCES `YouTube`.`Suscripciones` (`id_suscripcion`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_usuarios_playList`
-    FOREIGN KEY (`id_playList`)
-    REFERENCES `YouTube`.`PlayList` (`id_playList`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id_usuario`))
 ENGINE = InnoDB;
 
 
@@ -125,8 +37,8 @@ ENGINE = InnoDB;
 -- Table `YouTube`.`Videos`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `YouTube`.`Videos` (
-  `id_video` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_usuario` SMALLINT UNSIGNED NOT NULL,
+  `id_video` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT UNSIGNED NOT NULL,
   `titulo` VARCHAR(45) NOT NULL,
   `descripcion` VARCHAR(45) NOT NULL,
   `tamaño` VARCHAR(45) NOT NULL,
@@ -151,15 +63,185 @@ ENGINE = InnoDB;
 -- Table `YouTube`.`Etiquetas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `YouTube`.`Etiquetas` (
-  `id_etiqueta` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `id_video` SMALLINT UNSIGNED NOT NULL,
+  `id_etiqueta` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT NOT NULL,
   `nombre_etiqueta` VARCHAR(45) NOT NULL,
   `fecha_hora` DATETIME NOT NULL,
-  PRIMARY KEY (`id_etiqueta`),
-  INDEX `fk_etiquetas_videos_idx` (`id_video` ASC) ,
-  CONSTRAINT `fk_etiquetas_videos`
+  PRIMARY KEY (`id_etiqueta`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `YouTube`.`Canales`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `YouTube`.`Canales` (
+  `id_canal` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT UNSIGNED NOT NULL,
+  `nombre_canal` VARCHAR(45) NOT NULL,
+  `descripcion` VARCHAR(45) NOT NULL,
+  `fecha_creacion` DATE NOT NULL,
+  PRIMARY KEY (`id_canal`),
+  INDEX `fk_canal_usuario_idx` (`id_usuario` ASC) ,
+  CONSTRAINT `fk_canal_usuario`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `YouTube`.`Usuarios` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `YouTube`.`PlayList`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `YouTube`.`PlayList` (
+  `id_playList` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT UNSIGNED NOT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `fecha_creacion` DATE NOT NULL,
+  `estado` ENUM('publico', 'privado') NOT NULL,
+  PRIMARY KEY (`id_playList`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `YouTube`.`Comentarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `YouTube`.`Comentarios` (
+  `id_comentario` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_video` INT UNSIGNED NOT NULL,
+  `id_usuario` INT UNSIGNED NOT NULL,
+  `texto_comentario` VARCHAR(100) NOT NULL,
+  `fecha_hora` DATETIME NOT NULL,
+  PRIMARY KEY (`id_comentario`),
+  INDEX `fk_comentarios_videos_idx` (`id_video` ASC) ,
+  INDEX `fk_comentarios_usuarios_idx` (`id_usuario` ASC) ,
+  CONSTRAINT `fk_comentarios_videos`
     FOREIGN KEY (`id_video`)
     REFERENCES `YouTube`.`Videos` (`id_video`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comentarios_usuarios`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `YouTube`.`Usuarios` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `YouTube`.`Suscripciones`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `YouTube`.`Suscripciones` (
+  `id_suscripcion` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_canal` INT UNSIGNED NOT NULL,
+  `id_usuario` INT UNSIGNED NOT NULL,
+  `fecha_suscripcion` DATE NULL,
+  PRIMARY KEY (`id_suscripcion`),
+  INDEX `fk_suscripciones_usuarios_idx` (`id_usuario` ASC) ,
+  INDEX `fk_suscripciones_canales_idx` (`id_canal` ASC) ,
+  CONSTRAINT `fk_suscripciones_usuarios`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `YouTube`.`Usuarios` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_suscripciones_canales`
+    FOREIGN KEY (`id_canal`)
+    REFERENCES `YouTube`.`Canales` (`id_canal`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `YouTube`.`Likes_dislikes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `YouTube`.`Likes_dislikes` (
+  `id_Likes_dislikes` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT UNSIGNED NOT NULL,
+  `id_video` INT UNSIGNED NOT NULL,
+  `like` ENUM("1", "0") NOT NULL,
+  `fecha_creacion` DATE NOT NULL,
+  PRIMARY KEY (`id_Likes_dislikes`),
+  INDEX `fk_mgNmg_video_idx` (`id_video` ASC) ,
+  INDEX `fk_mgNmg_usuario_idx` (`id_usuario` ASC) ,
+  CONSTRAINT `fk_mgNmg_video`
+    FOREIGN KEY (`id_video`)
+    REFERENCES `YouTube`.`Videos` (`id_video`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_mgNmg_usuario`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `YouTube`.`Usuarios` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `YouTube`.`Etiquetas_Videos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `YouTube`.`Etiquetas_Videos` (
+  `id_etiquetas_videos` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_video` INT UNSIGNED NOT NULL,
+  `id_etiqueta` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`id_etiquetas_videos`),
+  INDEX `fk_etiVid_video_idx` (`id_video` ASC) ,
+  INDEX `fk_etiVid_etiqueta_idx` (`id_etiqueta` ASC) ,
+  CONSTRAINT `fk_etiVid_video`
+    FOREIGN KEY (`id_video`)
+    REFERENCES `YouTube`.`Videos` (`id_video`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_etiVid_etiqueta`
+    FOREIGN KEY (`id_etiqueta`)
+    REFERENCES `YouTube`.`Etiquetas` (`id_etiqueta`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `YouTube`.`PlayList_Videos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `YouTube`.`PlayList_Videos` (
+  `idPlayList_Videos` INT NOT NULL AUTO_INCREMENT,
+  `id_playlist` INT UNSIGNED NOT NULL,
+  `id_videos` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`idPlayList_Videos`),
+  INDEX `fk_PlayVid_playlist_idx` (`id_playlist` ASC) ,
+  INDEX `fk_PlayVid_videos_idx` (`id_videos` ASC) ,
+  CONSTRAINT `fk_PlayVid_playlist`
+    FOREIGN KEY (`id_playlist`)
+    REFERENCES `YouTube`.`PlayList` (`id_playList`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PlayVid_videos`
+    FOREIGN KEY (`id_videos`)
+    REFERENCES `YouTube`.`Videos` (`id_video`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `YouTube`.`comentarios_likes_dislikes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `YouTube`.`comentarios_likes_dislikes` (
+  `id_comentarios_likes_dislikes` INT NOT NULL AUTO_INCREMENT,
+  `id_usuario` INT UNSIGNED NOT NULL,
+  `id_comentario` INT UNSIGNED NOT NULL,
+  `like` ENUM("1", "0") NULL,
+  PRIMARY KEY (`id_comentarios_likes_dislikes`),
+  INDEX `fk_comLikDislik_usuario_idx` (`id_usuario` ASC) ,
+  INDEX `fk_comLikDislik_comentario_idx` (`id_comentario` ASC) ,
+  CONSTRAINT `fk_comLikDislik_usuario`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `YouTube`.`Usuarios` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comLikDislik_comentario`
+    FOREIGN KEY (`id_comentario`)
+    REFERENCES `YouTube`.`Comentarios` (`id_comentario`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
